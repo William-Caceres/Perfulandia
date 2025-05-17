fa219f0e64e1feb23284da1345057b02f2946739
LucianoEscalona <luci.escalona@duocuc.cl>
2025-05-17 05:27:15 -0400

    perfuV1.0
    Se crea la carpeta para trabajar el proyecto

---
+++ Perfulandia/src/test/java/proyecto/Perfulandia/PerfulandiaApplicationTests.java
@@ -0,0 +1,13 @@
+package proyecto.Perfulandia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
