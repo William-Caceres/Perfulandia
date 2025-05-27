fbb16a938cc4af737472b32a9c2b0c08f5116c67
William-Caceres <wi.caceres@duocuc.cl>
2025-05-27 17:31:53 -0400

    Perfulandia_V1.29(27/05)
    Creacion Incidencia.js

---
+++ Perfulandia/src/main/resources/static/js/app_incidencia.js
@@ -0,0 +1,12 @@
+function registrarIncidencia(){
+    fetch("http://localhost:8080//api/v2/incidencia", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            correo:   document.getElementById("correo").value,
+            comentario:    document.getElementById("comentario").value
+
+        })
+    })  .then(res => res.json())
+        .then(data => alert("Formulario enviado correctamente con la id: " + data.id));
+}
