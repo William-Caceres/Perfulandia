73c43c40dc08b55e989ea9d4605b4a0cafef4fc1
William-Caceres <wi.caceres@duocuc.cl>
2025-05-27 17:21:36 -0400

    Perfulandia_V1.28(27/05)
    creacion Incidencia Controller

--- Perfulandia/src/main/java/proyecto/Perfulandia/Controller/IncidenciaController.java
+++ Perfulandia/src/main/java/proyecto/Perfulandia/Controller/IncidenciaController.java
@@ -1,7 +1,25 @@
 package proyecto.Perfulandia.Controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import proyecto.Perfulandia.Model.Incidencia;
+
+import proyecto.Perfulandia.Service.IncidenciaService;;
+
+@RestController
+@RequestMapping("/api/v2/incidencia")
 public class IncidenciaController {
+    @Autowired
+    private IncidenciaService inciServ;
+    //Agregar incidencia
+    @PostMapping
+    public Incidencia agregarIncidencia(@RequestBody Incidencia incidencia){
+        return inciServ.saveIncidencia(incidencia);
+    }
     
 }
