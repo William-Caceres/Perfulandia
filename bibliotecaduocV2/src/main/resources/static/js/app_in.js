9bf4bb0ec55aff82d7b903036470ece29187ea16
William-Caceres <wi.caceres@duocuc.cl>
2025-05-29 16:32:49 -0400

    EliminacionBibliotecaDuocReferencial

--- bibliotecaduocV2/src/main/resources/static/js/app_in.js
+++
@@ -1,8 +0,0 @@
-const nombre = sessionStorage.getItem("nombreUsuario");
-    if (nombre){
-        document.getElementById("mensaje").textContent = `Bienvenid@, ${nombre}`;
-    }
-    function cerrarSesion(){
-        sessionStorage.clear();
-        window.location.href = "login.html"
-    }
