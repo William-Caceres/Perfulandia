4448e9f0c4515f6b2bb04ff45b5b1c56a5271938
William-Caceres <wi.caceres@duocuc.cl>
2025-06-06 16:37:27 -0400

    PerfulandiaV2.1
    Creacion de directorio para insertar imagenes de productos

--- Perfulandia/src/main/resources/static/js/app.js
+++ Perfulandia/src/main/resources/static/js/app.js
@@ -21,6 +21,7 @@ function listarProductos() {
                                 <p class="card-text">contenido: ${producto.ml}ml</p>
                                 <p class="card-text">Stock disponible: ${producto.stock}</p>
                                 <h5 class="card-text">$${producto.precio}</h5>
+                                <img src="imagenes/perfume1.jpg" width="100%" height="200">
                             </div>
                             <div class="card-footer">
                                 <button class="btn btn-success btn-sm" onclick="carrito.agregarProducto(${producto.id})">Agregar al carro</button>
